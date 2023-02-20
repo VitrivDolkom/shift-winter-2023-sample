238d6fc2c4fe190041289647ff8b9e0a3249d447
Volkov Dmitriy <vdy0555@gmail.com>
2023-02-20 16:28:51 +0700

    added backend instructtions in readme file

--- src/utils/api/pizzaApi.ts
+++ src/utils/api/pizzaApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://shift-winter-2023-backend.onrender.com/api/pizza'
+const BASE_URL = 'http://localhost:3000/api/pizza'
 
 export const pizzaIstance = axios.create({
     baseURL: BASE_URL,
